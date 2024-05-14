03c8ebe44a62ce63a507e051106b892f1345281e
sagar gurjar <sagarchhalotre@gmail.com>
2024-05-14 16:30:45 +0530

    first commit

--- EMTA_AWS/settings.py
+++ EMTA_AWS/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile123')
 
 STATICFILES_DIRS = (
                         os.path.join(BASE_DIR, 'static'),
