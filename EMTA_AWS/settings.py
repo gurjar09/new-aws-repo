ffbc9c5b377af17667a8ce7c526b428c3239d7ff
sagar gurjar <sagarchhalotre@gmail.com>
2024-05-15 11:59:21 +0530

    Saving untracked changes

--- EMTA_AWS/settings.py
+++ EMTA_AWS/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/ubuntu/EMTA/new-aws-repo/staticfiles/'
-STATICFILES_DIRS = ( 
-        os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = ( 
+#         os.path.join(BASE_DIR, 'static'),
+# )
 
 
 MEDIA_URL = '/media/'
