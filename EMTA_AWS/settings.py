f4474e2fc0f20e0728e327da18cb4168b369956b
gurjar09 <sagarchhalotre@gmail.com>
2024-07-18 15:34:14 +0530

    Commit

--- EMTA_AWS/settings.py
+++ EMTA_AWS/settings.py
@@ -85,9 +85,10 @@ WSGI_APPLICATION = 'EMTA_AWS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
